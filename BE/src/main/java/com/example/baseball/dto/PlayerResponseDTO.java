e89d954d5edda3d34f64af6d1d9643bc9dde3721
Tree <gmldbs1109@naver.com>
2021-05-07 01:08:44 +0900

    feature : 필요한 정보가 response되도록 dto 구현

---
+++ BE/src/main/java/com/example/baseball/dto/PlayerResponseDTO.java
@@ -0,0 +1,49 @@
+package com.example.baseball.dto;
+
+public class PlayerResponseDTO {
+    private String name;
+    private String position;
+    private String atBat; //타석
+    private int hits;
+    private int out;
+    private double battingAverage; //타율
+    private int numberOfPitches; //투구수
+
+    public PlayerResponseDTO(String name, String position, String atBat, int hits, int out, double battingAverage, int numberOfPitches) {
+        this.name = name;
+        this.position = position;
+        this.atBat = atBat;
+        this.hits = hits;
+        this.out = out;
+        this.battingAverage = battingAverage;
+        this.numberOfPitches = numberOfPitches;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getAtBat() {
+        return atBat;
+    }
+
+    public int getHits() {
+        return hits;
+    }
+
+    public int getOut() {
+        return out;
+    }
+
+    public double getBattingAverage() {
+        return battingAverage;
+    }
+
+    public int getNumberOfPitches() {
+        return numberOfPitches;
+    }
+}
