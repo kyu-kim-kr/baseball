13ddb85a5125bf2da63c781dce0c9afdfac94b51
Tree <gmldbs1109@naver.com>
2021-05-08 02:25:28 +0900

    Feature : ScoreRepository 메소드 추가

--- BE/src/main/java/com/example/baseball/repository/ScoreRepository.java
+++ BE/src/main/java/com/example/baseball/repository/ScoreRepository.java
@@ -3,5 +3,8 @@ package com.example.baseball.repository;
 import com.example.baseball.entity.Score;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ScoreRepository extends CrudRepository<Score, Long> {
+    Optional<Score> findByHomeTeamAndAwayTeam(String homeTeam, String awayTeam);
 }
